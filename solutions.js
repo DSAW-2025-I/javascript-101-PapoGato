2e046506e64c1be1b0d7cabce400c98701eda149
PapoGato <sga200604@gmail.com>
2025-02-12 09:05:25 -0500

    added functions

--- solutions.js
+++ solutions.js
@@ -1,26 +1,50 @@
 // Sum of Two Numbers
 function sum(a, b) {
-  // Add your code here
+  let total=a+b;
+  return total;
 }
 
 // Factorial of a Number
 function factorial(n) {
-  // Add your code here
+  if (n < 0) return "El factorial no está definido para números negativos.";
+  let total = 1;
+  for (let i = 2; i <= n; i++) {
+    total *= i;
+  }
+  return total;
 }
 
 // Find the Largest Number
 function findLargest(arr) {
-  // Add your code here
+  if (arr.length === 0) return "El arreglo está vacío.";
+  const mayor = Math.max(...arr); 
+  return mayor;
 }
 
 // Count Vowels in a String
 function countVowels(str) {
-  // Add your code here
+  const vocales = 'aeiouAEIOU';
+    let cantidad=0;
+    for(let i=0;i<str.length;i++){
+      if(vocales.indexOf(str[i])!=-1){
+        cantidad++;
+    }
+  }
+  return cantidad;
 }
 
 // Check if a Number is Prime
 function isPrime(n) {
-  // Add your code here
+  if (n <= 1) return false;
+  if (n === 2) return true;
+  if (n % 2 === 0) return false;
+  for (let i = 3; i <= Math.sqrt(n); i += 2) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 module.exports = {
